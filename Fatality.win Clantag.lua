c6589df1fe5df4053709bc76f736e5749bc99c1c
プレイヤー-2 <95395808+gadiiell@users.noreply.github.com>
2023-02-07 20:43:23 -0600

    Create Fatality.win Clantag.lua

---
+++ Fatality.win Clantag.lua
@@ -0,0 +1,43 @@
+Made by gadiiell_
+local labels = {
+   "  ",
+"                  ",
+"                 f",
+"                fa",
+"               fat",
+"              fata",
+"             fatal",
+"            fatali",
+"           fatalit",
+"          fatality",
+"         fatality",
+"        fatality| ",
+"       fatality ",
+"      fatality|  ",
+"     fatality   " ,
+"    fatality|     " ,
+"   fatality      " ,
+"  fatalit       " ,
+" fatali        " ,
+"fatal         " ,
+"fata          " ,
+"fat           " ,
+"fa           " ,
+"f             "
+}
+local time = {
+    first = 0, second = 0
+}
+
+cheat.push_callback("on_paint", function()
+    if time.first < globalvars.get_tickcount() then
+        time.second = time.second + 1
+        if time.second > #labels + 1 then
+            time.second = 0
+        end
+
+        engine.set_clantag(labels[time.second])
+
+        time.first = globalvars.get_tickcount() + 17
+    end
+end)
