cfb29325e8907adb4cd4814d839dcde6d9632174
プレイヤー-2 <95395808+gadiiell@users.noreply.github.com>
2023-02-09 14:52:38 -0600

    Update Nazi clantag.lua

--- Nazi clantag.lua
+++ Nazi clantag.lua
@@ -1,27 +1,45 @@
 local labels = {
-	"卐",
-	"卍",
-	"卐",
-	"卍",
-	"卐",
-	"卍",
-	"卐",
-	"卍",
-	"卐",
-	"卍",
-	"卐",
-	"卍",
-	"卐",
-	"卍"
-}
+  "  ",
 
+  "卐",
+
+	"卍",
+
+ 	"卐",
+
+ 	"卍",
+
+ 	"卐",
+
+ 	"卍",
+
+ 	"卐",
+
+ 	"卍",
+
+ 	"卐",
+
+ 	"卍",
+
+ 	"卐",
+
+ 	"卍",
+
+ 	"卐",
+
+ 	"卍"
+
+
+
+
+}
 local time = {
-	first = 0, second = 0
+    first = 0, second = 0
 }
 
-cheat.RegisterCallback("on_paint", function()
-    if time.first < globalvars.get_tickcount() then   
-		time.second = time.second + 1
+cheat.push_callback("on_paint", function()
+    if time.first < globalvars.get_tickcount() then
+        time.second = time.second + 1
         if time.second > #labels + 1 then
             time.second = 0
         end
